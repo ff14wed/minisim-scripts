4739f7c9b2ef5692fb4f36d9b755d41149e6fdd5
Flawed <33593723+ff14wed@users.noreply.github.com>
2025-11-05 12:04:30 -0800

    Basic funky floor structure confirmed

--- savage/m5s/funky_floor.js
+++ savage/m5s/funky_floor.js
@@ -7,6 +7,11 @@ const STATUS_BURN_BABY_BURN = 214280;
 const STATUS_IN_THE_SPOTLIGHT = 214284;
 const STATUS_MAGIC_VULNERABILITY_UP = 215057;
 const STATUS_DAMAGE_DOWN = 215520;
+
+// =============================================================================
+// Public functions
+// =============================================================================
+
 /**
  * Returns the name of the encounter.
  * @return {string}
@@ -45,6 +50,40 @@ function used_status_effect_ids() {
     STATUS_DAMAGE_DOWN,
   ];
 }
+
+/**
+ * Asynchronous function that runs the encounter. All interactions with the
+ * world must be done via the provided commands object.
+ * @param {api.EncounterCommands} commands
+ */
+async function run(commands) {
+  await commands.sleep_until(2000);
+  disco_infernal(commands);
+
+  // Spawn spotlights at the end of the disco infernal animation
+  await commands.sleep_duration(1800);
+  execute_spotlights(commands);
+
+  // Start the Funky Floor timeline
+  alternate_floor_aoes(commands);
+
+  // End the encounter at 50 seconds
+  await commands.sleep_until(50000);
+  commands.finish_encounter();
+}
+
+// Export the public interface for this encounter script.
+globalThis.exports = {
+  encounter_name,
+  arena,
+  role_positions,
+  used_status_effect_ids,
+  run,
+};
+
+// =============================================================================
+// Private functions
+// =============================================================================
 
 /**
  * Spawns an AoE that players should avoid.
@@ -86,56 +125,6 @@ async function magic_damage(commands, role_snapshot) {
 }
 
 /**
- * Timeline:
- * 1:16.5 - Disco Infernal Cast - 3.70 s
- * time between cast-end and aoe is roughly 0.7s
- * disco infernal aoe is roughly 1.5s
- * 1:21.7 - Funky Floor Cast - 2.00s
- * 1:25.7 - Funky Floor Cast
- * 1:29.8 - Funky Floor Cast
- * 1:29.9 - Inside Out Cast - 4.70s
- * 1:32.3 - Other half of Inside Out
- * 1:33.8 - Funky Floor Cast
- * 1:37.8 - Funky Floor Cast
- * 1:41.9 - Funky Floor Cast
- * 1:46.0 - Funky Floor Cast
- * 1:50.0 - Funky Floor Cast
- * 1:54.1 - Funky Floor Cast
- * 1:58.2 - Funky Floor Cast
- *
- * 20 frame delay between telegraph and aoe
- * 0.333s
- * 48 frames for aoe duration
- * 0.8s aoe duration
- * roughly 1s delay before next
- */
-
-/**
- * Asynchronous function that runs the encounter. All interactions with the
- * world must be done via the provided commands object.
- * @param {api.EncounterCommands} commands
- */
-async function run(commands) {
-  await commands.sleep_until(2000);
-  initial_raidwide(commands);
-  await commands.sleep_until(2500);
-
-  spawn_disco_circles(commands);
-  alternate_floor_aoes(commands, 2500);
-
-  await commands.sleep_until(60000);
-  commands.finish_encounter();
-}
-
-globalThis.exports = {
-  encounter_name,
-  arena,
-  role_positions,
-  used_status_effect_ids,
-  run,
-};
-
-/**
  * Kills the specified role if the provided expiration is allowed to go
  * to completion.
  * @param {api.EncounterCommands} commands
@@ -151,10 +140,10 @@ async function die_on_expiration(commands, role, expiration) {
  * Spawns the intial raidwide that gives the burn debuff.
  * @param {api.EncounterCommands} commands
  */
-async function initial_raidwide(commands) {
+async function disco_infernal(commands) {
   let id = await commands
     .build_aoe(api.Shape.circle(60.0))
-    .with_duration(1500)
+    .with_duration(1800)
     .execute();
   let snapshot = await commands.role_positions_snapshot(id);
   for (let role_snapshot of snapshot) {
@@ -171,44 +160,45 @@ async function initial_raidwide(commands) {
 }
 
 /**
- * Spawns all disco circles used in the encounter.
+ * Handles all spotlights used in the encounter.
  * @param {api.EncounterCommands} commands
  */
-async function spawn_disco_circles(commands) {
-  inside_disco_circle(commands, -2.5, -7.5, -2.5, 7.5);
-  inside_disco_circle(commands, 2.5, 7.5, 2.5, -7.5);
-  inside_disco_circle(commands, 7.5, -2.5, -7.5, -2.5);
-  inside_disco_circle(commands, -7.5, 2.5, 7.5, 2.5);
+async function execute_spotlights(commands) {
+  inside_spotlight(commands, -2.5, -7.5, -2.5, 7.5);
+  inside_spotlight(commands, 2.5, 7.5, 2.5, -7.5);
+  inside_spotlight(commands, 7.5, -2.5, -7.5, -2.5);
+  inside_spotlight(commands, -7.5, 2.5, 7.5, 2.5);
 }
 
 /**
- * Spawns the disco circle AoE, given point A and point B for where the disco
- * circle will travel
+ * Spawns the spotlight AoE, moves the entity over the course of the entire
+ * encounter, and spawns AoEs where the spotlight stops.
  * @param {api.EncounterCommands} commands
  * @param {number} a_x
  * @param {number} a_y
  * @param {number} b_x
  * @param {number} b_y
  */
-async function inside_disco_circle(commands, a_x, a_y, b_x, b_y) {
+async function inside_spotlight(commands, a_x, a_y, b_x, b_y) {
   let tfa = api.Transform2D.new_from_pos(api.mvec2(a_x, a_y));
   let tfb = api.Transform2D.new_from_pos(api.mvec2(b_x, b_y));
-  let dests = [tfb, tfa, tfb, tfa, tfb, tfa, tfb, tfa, tfb, tfa];
+  let dests = [tfb, tfa, tfb, tfa];
 
   let entity_id = await commands
     .build_telegraph(api.Shape.circle(2.5))
     .with_transform(tfa)
-    .with_duration(40000)
+    .with_duration(34467)
     .execute();
+  // Initial wait duration
+  await commands.sleep_duration(1533);
   for (let dest of dests) {
-    await commands.sleep_duration(2700);
     commands.build_auto_movement(entity_id, dest, 4000).execute();
     await commands.sleep_duration(4000);
     await commands
       .build_aoe(api.Shape.circle(2.5))
       .with_transform(dest)
       .execute();
-    await commands.sleep_duration(1300);
+    await commands.sleep_duration(4000);
   }
 }
 
@@ -216,12 +206,12 @@ async function inside_disco_circle(commands, a_x, a_y, b_x, b_y) {
  * @param {api.EncounterCommands} commands
  * @param {number} start_time in milliseconds
  */
-async function alternate_floor_aoes(commands, start_time) {
+async function alternate_floor_aoes(commands) {
   var shifted = false;
   for (let i = 0; i < 10; i++) {
-    await commands.sleep_until(start_time + i * 4000);
-    spawn_floor_aoes(commands, shifted);
+    spawn_floor_aoes(commands, shifted, i == 0);
     shifted = !shifted;
+    await commands.sleep_duration(4000);
   }
 }
 
@@ -231,18 +221,20 @@ async function alternate_floor_aoes(commands, start_time) {
  * @param {api.EncounterCommands} commands
  * @param {number} x
  * @param {number} y
+ * @param {boolean} is_first
  */
-async function spawn_floor_aoe(commands, x, y) {
+async function spawn_floor_aoe(commands, x, y, is_first) {
   let shape = api.Shape.rectangle(5.0, 5.0);
   let transform = api.Transform2D.new_from_pos(api.mvec2(x, y)).with_angle(
     Math.PI / 2
   );
+  let telegraph_duration = is_first ? 3000 : 2000;
   await commands
     .build_telegraph(shape)
     .with_transform(transform)
-    .with_duration(2000)
+    .with_duration(telegraph_duration)
     .execute();
-  await commands.sleep_duration(2700);
+  await commands.sleep_duration(telegraph_duration + 200);
   await spawn_aoe(commands, shape, transform);
 }
 
@@ -250,8 +242,9 @@ async function spawn_floor_aoe(commands, x, y) {
  * Spawns the telegraphs and AoEs for the Funky Floor AoEs.
  * @param {api.EncounterCommands} commands
  * @param {boolean} shifted
+ * @param {boolean} is_first
  */
-function spawn_floor_aoes(commands, shifted) {
+function spawn_floor_aoes(commands, shifted, is_first) {
   // Spawns squares that are 5 yalms wide in alternating fashion starting from
   // (-20, -20) to (20, 20)
   // The initial square is (-15, -17.5) because it is rotated sideways so that
@@ -267,7 +260,7 @@ function spawn_floor_aoes(commands, shifted) {
       if (spawn_here) {
         continue;
       }
-      spawn_floor_aoe(commands, x, y);
+      spawn_floor_aoe(commands, x, y, is_first);
     }
   }
 }
